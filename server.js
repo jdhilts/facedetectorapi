83aa7559254f705bb10e832daac853922900cb3d
jdhilts <jameshilts@icloud.com>
2020-05-18 19:07:50 -0700

    Corrected connectionString property in knex

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const database = knex({
 	client: 'pg',
 	connection: {
 		connectionString : process.env.DATABASE_URL,
-		ssl: true,
+		ssl: {
+			rejectUnauthorized: false
+		}
 	}});
 
 const app = express();
